9b2d8083ecd94e3989b6ff39828d2eccff15cc3f
John Stephen <wzxjohn@users.noreply.github.com>
2015-01-10 17:08:20 +0800

    Rename files and modify login page

--- application/views/admin_login.php
+++ application/views/admin_login.php
@@ -7,6 +7,7 @@
  */
 
 defined('BASEPATH') OR exit('No direct script access allowed');
+$this->load->helper('url');
 ?>
 <!DOCTYPE html>
 <html class="bg-black">
@@ -27,20 +28,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
 </head>
-
-
 <body class="bg-black">
-
 <div class="form-box" id="login-box">
-
     <div class="header">登录</div>
     <form role="form" action="" method="post" onsubmit="return logincheck()">
         <div class="body bg-gray">
             <div class="form-group">
-                <input type="username"  name="username" class="form-control" placeholder="Username" required autofocus>
+                <input type="username"  id="username" name="username" class="form-control" placeholder="Username" required autofocus>
             </div>
             <div class="form-group">
-                <input type="password"  name="password" class="form-control" placeholder="Password" required>
+                <input type="password"  id="password" name="password" class="form-control" placeholder="Password" required>
             </div>
             <div class="form-group">
                 <input type="checkbox" name="remember_me" value="week"/> 保存Cookie7天
@@ -50,21 +47,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <button type="submit" class="btn bg-olive btn-block"  name="login" >登录</button>
         </div>
     </form>
-
 </div>
-
 <script LANGUAGE="javascript">
     function logincheck()
     {
-        if(document.login.username.value.length==0){
+        if(document.getElementById('username').value.length==0){
             alert("请输入用户名");
-            document.login.username.focus();
+            document.getElementById('username').focus();
             return false;
         }
 
-        if(document.login.password.value.length==0){
+        if(document.getElementById('password').value.length==0){
             alert("请输入密码!");
-            document.login.password.focus();
+            document.getElementById('password').focus();
             return false;
         }
     }
