5f0e72c12fb13542ff51cc30fa1c823882a66070
John Stephen <wzxjohn@users.noreply.github.com>
2015-01-11 17:43:20 +0800

    Modify site index

--- application/controllers/SiteIndex.php
+++ application/controllers/SiteIndex.php
@@ -8,7 +8,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Index extends CI_Controller {
+class SiteIndex extends CI_Controller {
 
     public function index()
     {
