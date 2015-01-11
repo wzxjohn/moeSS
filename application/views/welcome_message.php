41ba620783f577eb5a18fac0bc4e89b953ab0896
John Stephen <wzxjohn@users.noreply.github.com>
2015-01-11 16:34:08 +0800

    Modify admin login function

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -117,6 +117,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<code>application/controllers/Welcome.php</code>
 
 		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		<p><a href="<?php echo site_url('admin/logout'); ?>">Logout</a></p>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
