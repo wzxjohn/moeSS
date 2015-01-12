c29f7dbf85324d7be9d318b10e98f2d021ebb787
John Stephen <wzxjohn@users.noreply.github.com>
2015-01-13 01:12:46 +0800

    Modify user functions

--- application/views/user/user_footer.php
+++ application/views/user/user_footer.php
@@ -31,15 +31,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!-- iCheck -->
 <script src="<?php echo base_url('static/js/plugins/iCheck/icheck.min.js'); ?>" type="text/javascript"></script>
 
-<!-- AdminLTE App -->
-<script src="<?php echo base_url('static/js/AdminLTE/app.js'); ?>" type="text/javascript"></script>
-
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="<?php echo base_url('static/js/AdminLTE/dashboard.js'); ?>" type="text/javascript"></script>
-
-<!-- AdminLTE for demo purposes -->
-<script src="<?php echo base_url('static/js/AdminLTE/demo.js'); ?>" type="text/javascript"></script>
-
 <!-- Select js -->
 <script src="<?php echo base_url('static/js/bootstrap-select.js'); ?>"></script>
 <script src="<?php echo base_url('static/js/bootstrap-switch.js'); ?>"></script>
