cc05f9d29bd479c2b48c357a320401a2c1be5909
wzxjohn <wzxjohn@users.noreply.github.com>
2015-01-16 19:51:06 +0800

    Modify config edit function

--- application/views/admin/admin_config.php
+++ application/views/admin/admin_config.php
@@ -29,60 +29,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     <!-- form start -->
                     <form role="form" method="post" action="<?php echo site_url( "admin/config_update"); ?>">
                         <div class="box-body">
+                        <?php foreach ($configs as $config) : ?>
                             <div class="form-group">
-                                <label for="cate_title">用户名</label>
-                                <input type="text" class="form-control" name="user_name" <?php if ($user) {echo "value=\"$user->user_name\""; } ?> >
+                                <label for="cate_title"><?php echo $config->display_name; ?><small><?php echo $config->option_name; ?></small></label>
+                                <input type="text" class="form-control" name="<?php echo $config->option_name; ?>" value="<?php echo $config->option_value; ?>" >
                             </div>
-
-                            <div class="form-group">
-                                <label for="cate_title">E-mail</label>
-                                <input type="email" class="form-control" name="email" <?php if ($user) {echo "value=\"$user->email\""; } ?> >
-                            </div>
-
-                            <div class="form-group">
-                                <label for="cate_title">密码</label>
-                                <input type="text" class="form-control" name="pass" <?php if ($user) {echo "value=\"$user->pass\""; } ?> >
-                            </div>
-
-                            <div class="form-group">
-                                <label for="cate_title">SS密码</label>
-                                <input type="text" class="form-control" name="passwd" <?php if ($user) {echo "value=\"$user->passwd\""; } ?> >
-                            </div>
-
-                            <div class="form-group">
-                                <label for="cate_order">上传</label>
-                                <input type="text" class="form-control" name="u" <?php if ($user) {echo "value=\"$user->u\""; } ?> >
-                            </div>
-
-                            <div class="form-group">
-                                <label for="cate_order">下载</label>
-                                <input type="text" class="form-control" name="d" <?php if ($user) {echo "value=\"$user->d\""; } ?> >
-                            </div>
-
-                            <div class="form-group">
-                                <label for="cate_order">总流量</label>
-                                <input type="text" class="form-control" name="transfer_enable" <?php if ($user) {echo "value=\"$user->transfer_enable\""; } ?> >
-                            </div>
-
-                            <div class="form-group">
-                                <label for="cate_order">套餐</label>
-                                <input type="text" class="form-control" name="plan" <?php if ($user) {echo "value=\"$user->plan\""; } ?> >
-                            </div>
-
-                            <div class="form-group">
-                                <label for="cate_order">端口</label>
-                                <input type="text" class="form-control" name="port" <?php if ($user) {echo "value=\"$user->port\""; } ?> >
-                            </div>
-
-                            <div class="form-group">
-                                <label for="cate_order">开关</label>
-                                <input type="text" class="form-control" name="switch" <?php if ($user) {echo "value=\"$user->switch\""; } ?> >
-                            </div>
-
-                            <div class="form-group">
-                                <label for="cate_order">启用</label>
-                                <input type="text" class="form-control" name="enable" <?php if ($user) {echo "value=\"$user->enable\""; } ?> >
-                            </div>
+                        <?php endforeach ?>
                         </div><!-- /.box-body -->
                         <div class="box-footer">
                             <button type="submit" name="action" value="add" class="btn btn-primary">提交</button>
