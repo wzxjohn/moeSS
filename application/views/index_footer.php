8a15155efa342f92c71bcd95a244529a451e3311
wzxjohn <wzxjohn@gmail.com>
2015-01-17 23:38:56 +0800

    Delete guestbook

--- application/views/index_footer.php
+++ application/views/index_footer.php
@@ -9,5 +9,5 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <div class="footer">
-    <p>&copy; <?php echo SITE_NAME." &#169;".date('Y'); ?>  Power by <a href="https://github.com/wzxjohn/moeSS" target="_blank">moeSS</a>&nbsp;<a href="<?php echo site_url('user/guestbook'); ?>">讨论</a></p>
+    <p>&copy; <?php echo SITE_NAME." &#169;".date('Y'); ?>  Power by <a href="https://github.com/wzxjohn/moeSS" target="_blank">moeSS</a></p>
 </div>
