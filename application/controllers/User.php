7cacc7a46773382aa9162a1640b6e54893667f5d
John Stephen <wzxjohn@users.noreply.github.com>
2015-01-17 17:06:35 +0800

    Modify send mail function

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -566,7 +566,7 @@ class User extends CI_Controller
             $code = $data['activate_code'];
             $subject = $this->user_model->get_email_subject();
             $html = $this->user_model->get_email_templates();
-            $html = sprintf($html, site_url("user/active/$code"));
+            $html = sprintf($html, site_url("user/activate/$code"));
             $this->load->helper('comm');
             if (send_mail(null,null,$email,$subject,$html))
             {
