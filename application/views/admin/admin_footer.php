020f900881099287b8a7d616ee06feb06c66e23e
John Stephen <wzxjohn@users.noreply.github.com>
2015-01-24 16:01:04 +0800

    Remove unused js

--- application/views/admin/admin_footer.php
+++ application/views/admin/admin_footer.php
@@ -40,49 +40,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><script src="<?php echo base_url('static/js/jquery-2.1.1.js'); ?>"></script>
 <script src="<?php echo base_url('static/js/bootstrap.min.js'); ?>" type="text/javascript"></script>
 <script src="<?php echo base_url('static/js/jquery-ui.min.js'); ?>" type="text/javascript"></script>
-<!-- Morris.js charts -->
-<script src="<?php echo base_url('static/js/raphael-min.js'); ?>"></script>
-<script src="<?php echo base_url('static/js/plugins/morris/morris.min.js'); ?>" type="text/javascript"></script>
-<!-- Sparkline -->
-<script src="<?php echo base_url('static/js/plugins/sparkline/jquery.sparkline.min.js'); ?>" type="text/javascript"></script>
-<!-- jvectormap -->
-<script src="<?php echo base_url('static/js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js'); ?>" type="text/javascript"></script>
-<script src="<?php echo base_url('static/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js'); ?>" type="text/javascript"></script>
-<!-- jQuery Knob Chart -->
-<script src="<?php echo base_url('static/js/plugins/jqueryKnob/jquery.knob.js'); ?>" type="text/javascript"></script>
-<!-- Moment JS -->
-<script src="<?php echo base_url('static/js/plugins/moment/moment.min.js'); ?>"></script>
-<!-- daterangepicker -->
-<script src="<?php echo base_url('static/js/plugins/daterangepicker/daterangepicker.js'); ?>" type="text/javascript"></script>
-<!-- datepicker -->
-<script src="<?php echo base_url('static/js/plugins/datepicker/bootstrap-datepicker.js'); ?>" type="text/javascript"></script>
-<!-- dateTable -->
-<script src="<?php echo base_url('static/js/plugins/datatables/dataTables.bootstrap.js'); ?>" type="text/javascript"></script>
-<!-- Bootstrap WYSIHTML5 -->
-<script src="<?php echo base_url('static/js/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js'); ?>" type="text/javascript"></script>
-<!-- iCheck -->
-<script src="<?php echo base_url('static/js/plugins/iCheck/icheck.min.js'); ?>" type="text/javascript"></script>
 <!-- AdminLTE App -->
 <script src="<?php echo base_url('static/js/AdminLTE/app.js'); ?>" type="text/javascript"></script>
-
-<!-- Select js -->
-<script src="<?php echo base_url('static/js/bootstrap-select.js'); ?>"></script>
-<script src="<?php echo base_url('static/js/bootstrap-switch.js'); ?>"></script>
 <script src="<?php echo base_url("static/bootstrap-dialog/js/bootstrap-dialog.min.js"); ?>"></script>
-
-<script type="text/javascript">
-    $(window).on('load', function () {
-
-        $('.selectpicker').selectpicker({
-            'selectedText': 'cat'
-        });
-
-        // $('.selectpicker').selectpicker('hide');
-    });
-    $(function() {
-        $("#userTable").dataTable();
-    });
-</script>
 <?php $this->load->view('ana') ;?>
 </body>
 </html>
